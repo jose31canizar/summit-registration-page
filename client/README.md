0f5dc5326dc18c61a05077a4306c827b58ac39c9
jose31canizar <jose.canizares@colorado.edu>
2017-09-16 09:29:06 -0600

    create express backend

---
+++ client/README.md
@@ -0,0 +1,3 @@
+#Self-Transformation Summit Registration Site
+
+A summit event for those seeking to conquer food and body.
---
+++ client/src/components/Registration/Registration.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import NavBar from '../NavBar/NavBar'
+import './Registration.styl'
+
+class Registration extends Component {
+  render() {
+    return (
+      <div className="registration">
+        <NavBar />
+      </div>
+    );
+  }
+}
+
+export default Registration;
