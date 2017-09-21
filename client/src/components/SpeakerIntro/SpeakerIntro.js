f0ab1eec67f9332a67bb1276a4fea7f743f48690
jose31canizar <jose.canizares@colorado.edu>
2017-09-20 20:23:20 -0600

    fixed call to action on mobile

--- client/src/components/SpeakerIntro/SpeakerIntro.js
+++ client/src/components/SpeakerIntro/SpeakerIntro.js
@@ -4,10 +4,9 @@ import './SpeakerIntro.styl'
 class SpeakerIntro extends Component {
   render() {
     return (
-      <div className="speaker-intro">
+      <div className="speaker-intro" style={{backgroundImage: `url(${require('../../img/background.png')})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
         <h2>{this.props.data.title}</h2>
         <h3>{this.props.data.subtitle}</h3>
-        <img src={require('../../img/background.png')} />
       </div>
     );
   }
